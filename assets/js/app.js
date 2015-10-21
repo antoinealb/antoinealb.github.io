a1acf76eee1c06663e7c983e9b0b5a692cd0a0e5
Antoine Albertelli <antoinea101@gmail.com>
2015-10-21 20:59:27 +0200

    Remove useless fade effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,5 @@
 $( document ).ready(function() {
-
 	/* Sidebar height set */
+    /* TODO: Do this in CSS. */
 	$('.sidebar').css('min-height',$(document).height());
-
-	/* Secondary contact links */
-	var scontacts = $('#contact-list-secondary');
-	var contact_list = $('#contact-list');
-	
-	scontacts.hide();
-	
-	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
-	
-	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
-
 });
